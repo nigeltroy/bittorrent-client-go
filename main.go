95073d98246e07c9153c996feb41e87597f7aec4
Nigel Thomas Roy <nigel.troy1@gmail.com>
2022-01-09 01:34:07 -0500

    Add basic logging to file functionality

--- main.go
+++ main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"os"
+	"time"
+)
+
+const logFileName string = "logs.txt"
+
+func main() {
+	logFile, err := os.OpenFile(logFileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer logFile.Close()
+	log.SetOutput(logFile)
+	log.Printf("New program run at: %s\n\n", time.Now().String())
+}
